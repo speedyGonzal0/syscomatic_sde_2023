ba38b049217ef062a3d5c3844e7022ae7020a654
Samin Ahsan <turzo68@gmail.com>
2023-08-03 10:59:32 +0600

    cleanup

--- src/store/orderByOutlet.js
+++ src/store/orderByOutlet.js
@@ -1,8 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-const initialState = [{_id: null, outletName: 0, totalOrderAmount: 0}];
-
 const ordersByOutletSlice = createSlice({
     name: "obo",
     initialState: {
